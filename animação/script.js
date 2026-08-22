62eae0b10b485aee4a512ae2dfa02d5d5bddf250
MariaRita4 <mariaritalbuquerque2010@gmail.com>
2026-08-22 18:38:36 -0300

    correção1

--- animação/script.js
+++ animação/script.js
@@ -1,4 +1,8 @@
  const card = document.querySelector("#card")
         card.addEventListener("click" , (e)=>{card.classList.toggle("flip")
 
+        });
+ const card1 = document.querySelector("#card1")
+        card1.addEventListener("click" , (e)=>{card1.classList.toggle("flip1")
+
         })
